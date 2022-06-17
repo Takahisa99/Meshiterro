5a07cef68db0998011c1c9d404b925eaf2bc08ad
Takahisa99 <106425553+Takahisa99@users.noreply.github.com>
2022-06-17 05:20:16 +0000

    更新

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -2,7 +2,8 @@ class PostImage < ApplicationRecord
 
 has_one_attached:image
 belongs_to :user
-
+has_many :post_comments, dependent: :destroy
+has_many :favorites, dependent: :destroy
 
   def get_image
     if image.attached?
@@ -10,6 +11,10 @@ belongs_to :user
     else
       'no_image.jpg'
     end
+  end
+
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
   end
 
 
